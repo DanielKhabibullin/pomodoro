44d9726e8698dffa04588922ca16359660708f26
Daniel Khabibullin <i6unny@gmail.com>
2023-01-31 13:22:47 +0500

    start timer added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './index.html';
 import './index.scss';
+
+import { initControl } from './modules/control';
+
+const initPomodoro = () => {
+	initControl();
+}
+
+initPomodoro();
---
+++ src/modules/state.js
@@ -0,0 +1,6 @@
+const WORK_TIME = 10.2;
+export const state = {
+	timeLeft: WORK_TIME * 60,
+	isActive: false,
+	timerId: 0
+};
